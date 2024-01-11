0c69c1b5848970cd75e65f3d977c8fb1df7a1d97
Angel Suarez <angel@rise-technologies.com>
2024-01-11 16:00:23 +0100

    added X-Total-Count to allowed headers case sensitive

--- server-nextjs/next.config.js
+++ server-nextjs/next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-//const nextConfig = {
-//
-//};
-//
-//module.exports = nextConfig;
-
 module.exports = {
   images: {
     domains: ["images.ctfassets.net"],
