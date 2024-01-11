61ba183fcddea2cbdb97d6e200cfe92f111533a2
Angel Suarez <angel@rise-technologies.com>
2024-01-11 15:11:19 +0100

    hack

--- frontend-app/src/Details.js
+++ frontend-app/src/Details.js
@@ -197,43 +197,45 @@ const Details = () => {
     <Layout
         path={['Home', 'Details']}
     >
-      {item ? (
-        <div className="container" style={{ 'color': 'white'  }}><h1 className="mb-4">{item["projectname"]}</h1>
-            <div>
-              <div className="row mt-4">
-                <div className="col-md-6">
-                  <p><b>Project Owner:</b> {item["projectowner"]}</p>
-                  <p><b>Offset Project Operator:</b> {item["offsetprojectoperator"]}</p>
-                  <p><b>Authorized Project Designee:</b> {item["authorizedprojectdesignee"]}</p>
-                  <p><b>Verifier:</b> {item["verifier"]}</p>
-                  <p><b>Estimated Annual Emission Reductions:</b> {item["estimatedannualemissionreductions"]}</p>
-                  <p><b>PERs:</b> {item["pers"]}</p>
-                  <p><b>Registry / ARB:</b> {item["registryarb"]}</p>
-                  <p><b>ARB Project Detail:</b> {item["arbprojectdetail"]}</p>
+    <div className="container" style={{ 'color': 'white'  }}>
+        {item ? (<div>
+                <h1 className="mb-4">{item["projectname"]}</h1>
 
+                  <div className="row mt-4">
+                    <div className="col-md-6">
+                      <p><b>Project Owner:</b> {item["projectowner"]}</p>
+                      <p><b>Offset Project Operator:</b> {item["offsetprojectoperator"]}</p>
+                      <p><b>Authorized Project Designee:</b> {item["authorizedprojectdesignee"]}</p>
+                      <p><b>Verifier:</b> {item["verifier"]}</p>
+                      <p><b>Estimated Annual Emission Reductions:</b> {item["estimatedannualemissionreductions"]}</p>
+                      <p><b>PERs:</b> {item["pers"]}</p>
+                      <p><b>Registry / ARB:</b> {item["registryarb"]}</p>
+                      <p><b>ARB Project Detail:</b> {item["arbprojectdetail"]}</p>
+
+                    </div>
+                    <div className="col-md-6">
+                      <p><b>ARB ID:</b> {item["arbid"]}</p>
+                      <p><b>Project Listed:</b> {item["projectlisted"]}</p>
+                      <p><b>Project Registered:</b> {item["projectregistered"]}</p>
+                      <p><b>CCB / Certifications:</b> {item["ccb_certifications"]}</p>
+                      <p><b>Project Type:</b> {item["projecttype"]}</p>
+                      <p><b>Registry Documents:</b> {item["registrydocuments"]}</p>
+                      <p><b>Project Website: </b>
+                        <Link href={item['projectwebsite']} target="_blank" >
+                            Visit Project Website
+                        </Link></p>
+                    </div>
+                  </div>
                 </div>
-                <div className="col-md-6">
-                  <p><b>ARB ID:</b> {item["arbid"]}</p>
-                  <p><b>Project Listed:</b> {item["projectlisted"]}</p>
-                  <p><b>Project Registered:</b> {item["projectregistered"]}</p>
-                  <p><b>CCB / Certifications:</b> {item["ccb_certifications"]}</p>
-                  <p><b>Project Type:</b> {item["projecttype"]}</p>
-                  <p><b>Registry Documents:</b> {item["registrydocuments"]}</p>
-                  <p><b>Project Website: </b>
-                    <Link href={item['projectwebsite']} target="_blank" >
-                        Visit Project Website
-                    </Link></p>
-                </div>
-              </div>
-            </div>
-            <div className="mt-4">
-                <h3></h3>
+              ) : (
                 <canvas id="myChart" width="400" height="200"></canvas>
-            </div>
+              )}
+        <div className="mt-4">
+            <h3></h3>
+            <canvas id="myChart" width="400" height="200"></canvas>
         </div>
-      ) : (
-        <canvas id="myChart" width="400" height="200"></canvas>
-      )}
+    </div>
+
     </Layout>
   );
 };
